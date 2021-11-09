dbb8fe0f9706516e3de0186c97921d7bf5ee3c3b
Shaun Russell <shaun.d.russell@gmail.com>
2021-11-09 17:51:48 -0500

    more tweaking

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,27 @@
 module ApplicationHelper
-  def current_path_in_locale(locale, params)
+  def current_path_in_locale(locale, options={})
     locale = nil if locale == :en
 
-    url_for(request.query_parameters.merge(locale: locale))
+    url_for(request.query_parameters.merge(locale: locale, **options))
+  end
+
+  def hreflang_tags
+    links = hreflang_locales.map do |locale|
+      tag.link(rel: "alternate", hreflang: locale.to_s.downcase, href: current_path_in_locale(locale, only_path: false))
+    end
+
+    links.prepend(tag.link(rel: "alternate", hreflang: "x-default", href: current_path_in_locale(:en, only_path: false)))
+
+    links.join.html_safe
+  end
+
+  def canonical_url_tag
+    tag.link(rel: "canonical", href: current_path_in_locale(I18n.locale, only_path: false))
+  end
+
+  private
+
+  def hreflang_locales
+    Routes::SCOPED_LOCALES - [I18n.locale]
   end
 end
